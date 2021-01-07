d1fae5e74af11613c1ab2ae22e7e6176112e8a57
kleimanmarek@gmail.com <kleimanmarek@gmail.com>
2021-01-07 01:10:52 +0100

    I add music and it works

--- first_try_sfml/main.cpp
+++ first_try_sfml/main.cpp
@@ -1,11 +1,15 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
+    sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
+        sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
-
+        
+        sf::Music music;
+        music.openFromFile("Dobry-den.ogg");
     while (window.isOpen())
     {
         sf::Event event;
@@ -13,10 +17,11 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+            
         }
-
         window.clear();
         window.draw(shape);
+        music.play();
         window.display();
     }
 
